92b0814eda00a40e690e74962bc49d16d4625b55
abr4hamw <abraham4woods@gmail.com>
2025-09-02 18:15:52 -0600

    Nombre y  umero de control centrado (El nomb re siendo más grande que el número de contro y cambiando el color)

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.practica1.ui.theme.Practica1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +28,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Practica1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    GreetingText(
+                        name = "Abraham Jiménez",
+                        ctrl = "22130808"
                     )
+
                 }
             }
         }
@@ -43,5 +56,28 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     Practica1Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun GreetingText(name:String, ctrl: String, modifier: Modifier = Modifier){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,)
+    {
+        Text(
+            text = name,
+            fontSize = 80.sp,
+            lineHeight = 70.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Magenta
+        )
+
+        Text (
+            text = ctrl,
+            fontSize = 40.sp,
+            color =  Color.Blue
+        )
+
     }
 }
